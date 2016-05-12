6bfec031de3f7c40a1f7d88a4c626870796a8eed
jingbolee <jingbolee@foxmail.com>
2016-05-12 15:43:53 +0800

    创建NewClient新闻客户端 添加资源文件 创建SplashActivity页面

---
+++ app/src/main/java/com/ljb/newsclient/activity/GuideActivity.java
@@ -0,0 +1,15 @@
+package com.ljb.newsclient.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ljb.newsclient.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
---
+++ app/src/main/java/com/ljb/newsclient/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ljb.newsclient.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ljb.newsclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
